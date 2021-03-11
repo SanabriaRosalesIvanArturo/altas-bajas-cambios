30228700fedafa1b3befbfefc9d4a2f1bbd94ef3
IvanSR <navi.sr.2018@gmail.com>
2021-03-11 17:43:37 -0600

    build

---
+++ precache-manifest.10c5b89f5639bef369b53fda9d878005.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d477403bab8ee3e3b2996e1a0b4c0497",
+    "url": "/19feb1/index.html"
+  },
+  {
+    "revision": "6c7b9dc3c5b8ae544070",
+    "url": "/19feb1/static/css/2.039bb34a.chunk.css"
+  },
+  {
+    "revision": "94da2434bc3b4d4d53f5",
+    "url": "/19feb1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6c7b9dc3c5b8ae544070",
+    "url": "/19feb1/static/js/2.2f62611b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/19feb1/static/js/2.2f62611b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94da2434bc3b4d4d53f5",
+    "url": "/19feb1/static/js/main.15f37777.chunk.js"
+  },
+  {
+    "revision": "ed48590615a433df9190",
+    "url": "/19feb1/static/js/runtime-main.6efbdef3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/19feb1/static/media/logo.5d5d9eef.svg"
+  }
+]);
